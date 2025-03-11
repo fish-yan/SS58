3c485f19cd2502d5a682f55925d71823873e0c58
yan <15316488+fish-yan@users.noreply.github.com>
2025-03-11 14:26:58 +0800

    update test

--- Tests/SS58Tests/SS58Tests.swift
+++ Tests/SS58Tests/SS58Tests.swift
@@ -1,6 +1,11 @@
 import Testing
+import Foundation
 @testable import SS58
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Test func ss58Test() {
+    let publick = "0x928e3d9740e13980bfcbcd0f7c9571f82df0c283de5cbf0125f6295896e80e16"
+    let address = "14KAFEgpTDErZdUDzfrhnpeS1f4SLuMozkET3LrFuGaKmC3A"
+    let data = Data(hex: publick)
+    let result = try! SS58.encode(data: data, format: .polkadot)
+    #expect(result == address)
 }
